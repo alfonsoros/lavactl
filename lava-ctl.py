4b08aba5231e7ebc019f709f5055961f36f31afd
Alfonso Ros Dos Santos <alfonso.ros-dos-santos@evosoft.com>
2017-04-18 17:26:00 +0200

    Add flag to print the configuration to STDOUT

--- lava-ctl.py
+++ lava-ctl.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+
 from lava.Job import Job
 from lava.config.default import DefaultConfig
 
@@ -9,6 +11,7 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('kernel', help='Filepath to the Kernel.')
   parser.add_argument('filesystem', help='Filepath to the File System.')
+  parser.add_argument('--show-config', action='store_true', help='Print configuration')
 
   args = parser.parse_args()
 
@@ -16,6 +19,9 @@ if __name__ == '__main__':
   config.add_section('lava.files')
   config.set('lava.files', 'kernel', args.kernel)
   config.set('lava.files', 'filesystem', args.filesystem)
+
+  if args.show_config:
+    config.write(sys.stdout)
 
   job = Job(config)
   job.submit()
