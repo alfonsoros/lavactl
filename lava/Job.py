39d60e5edec674220d8a0423240330889cbafae9
Alfonso Ros Dos Santos <alfonso.ros-dos-santos@evosoft.com>
2017-04-19 10:52:09 +0200

    logging in Job.py

--- lava/Job.py
+++ lava/Job.py
@@ -5,6 +5,7 @@ import csv
 import xmlrpclib
 import time
 import urllib2
+import logging
 
 from progress.bar import Bar
 from jinja2 import Environment, PackageLoader
@@ -12,23 +13,32 @@ from lava.server.Storage import Storage
 from lava.server.interface import LavaRPC
 
 class Job(object):
-  def __init__(self, config):
+  def __init__(self, config, logger=None):
+    self._log = logger if logger else logging.getLogger('lava.job')
+
     self._lava_url = config.get('lava.server', 'url')
     self._sleep = config.getint('lava.jobs', 'sleep')
     self._running_timeout = config.getint('lava.jobs', 'running_timeout')
     self._waiting_timeout = config.getint('lava.jobs', 'waiting_timeout')
     self._env = Environment(loader=PackageLoader('lava.devices', 'templates'))
 
+    self._log.info('Lava URL:          %s', self._lava_url)
+    self._log.info('Waiting-loop time: %ds', self._sleep)
+    self._log.info('Running timemout:  %d', self._running_timeout)
+    self._log.info('Queued timeout:    %d', self._waiting_timeout)
+
+    self._log.debug('Uploading files to FTP server')
     with Storage(config) as ftp:
       self._kernel_url = ftp.upload(config.get('lava.files', 'kernel'))
       self._filesystem_url = ftp.upload(config.get('lava.files', 'filesystem'))
 
+    self._log.debug('Generating job description')
     qemu = self._env.get_template('qemux86.yaml')
-
     self._job_definition = qemu.render({
       'kernel_url' : self._kernel_url,
       'file_system_url' : self._filesystem_url,
     })
+
 
   def definition(self):
     return self._job_definition
@@ -36,22 +46,37 @@ class Job(object):
   def submit(self):
     with LavaRPC() as server:
       self._jobid = server.scheduler.submit_job(self._job_definition)
+      self._log.debug('Submitted Job ID: %d', self._jobid)
 
   def poll(self):
     with LavaRPC() as server:
 
-      wcount = 0 # number of loops while the job is running
-      count = 0  # number of loops waiting for the job to start
+      count = 0
+      status = server.scheduler.job_status(self._jobid).get('job_status')
 
-      bar = Bar('Polling job %d' % self._jobid, max=self._running_timeout)
-
-      while wcount < self._waiting_timeout and count < self._running_timeout:
-        status = server.scheduler.job_status(self._jobid)
-
+      # Wait while the job is Queued
+      bar = Bar('Job %d: waiting in queue ' % self._jobid,
+          max=self._waiting_timeout, suffix='%(index)d / %(max)d seconds')
+      while status == 'Submitted' and count < self._waiting_timeout:
         bar.index = count
         bar.update()
+        count += self._sleep
+        time.sleep(self._sleep)
+        status = server.scheduler.job_status(self._jobid).get('job_status')
 
-        if status['job_status'] == 'Complete':
+      # Job didn't start
+      if count >= self._waiting_timeout:
+        self._log.error('Waiting timeout for queued Job-ID %d', self._jobid)
+        exit(1)
+
+      count = 0
+      while count < self._running_timeout:
+        bar.message = 'Job %d: %s' % (self._jobid, status)
+        bar.index = count
+        bar.max = self._running_timeout
+        bar.update()
+
+        if status == 'Complete':
           bar.finish()
 
           # Check the tests passed
@@ -64,20 +89,23 @@ class Job(object):
           else:
             exit(1)
 
-        elif status['job_status'] == 'Canceled' or status['job_status'] == 'Cancelling':
+        elif status == 'Canceled' or status == 'Cancelling':
+            bar.finish()
             exit(1)
-        elif status['job_status'] == 'Submitted':
-            wcount += self._sleep
-        elif status['job_status'] == 'Running':
+
+        elif status == 'Running':
             count += self._sleep
-        elif status['job_status'] == 'Incomplete':
+
+        elif status == 'Incomplete':
+            bar.finish()
             exit(1)
 
         # Wait _sleep seconds
         time.sleep(self._sleep)
+        status = server.scheduler.job_status(self._jobid).get('job_status')
 
-      if wcount >= self._waiting_timeout:
-        log.error("Queued timeout")
-      else:
-        log.error("Running timeout")
-      exit(1)
+      bar.finish()
+
+      if count >= self._waiting_timeout:
+        self._log.error("Running timeout")
+        exit(1)
