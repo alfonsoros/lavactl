ad0f0cdc757ae0b8d418ea16f1b47b4e3a782fd6
Alfonso Ros Dos Santos <alfonso.ros-dos-santos@evosoft.com>
2017-10-02 16:48:17 +0200

    remove deprecated code from lava-ctl

--- lava-ctl.py
+++ lava-ctl.py
@@ -10,10 +10,6 @@ from pkg_resources import resource_filename
 from config import ConfigManager
 from commands import submit_job, upload_image, list_images, run_test
 
-from lava.server import LavaServer, FTPStorage
-from lava.jobs import JobDefinition
-from lava.tests import Test
-
 # Settup basic logging
 # TODO: Make lava-ctl to load the logging configuration from the conf file
 logging.basicConfig()
@@ -49,36 +45,6 @@ def overwrite_configuration_arguments(config, args):
     return config
 
 
-def test_local_image(arguments):
-    """Check if a local image is present in the arguments
-
-    :arguments: The command line arguments
-    :returns: TODO
-
-    """
-
-    parameters = [
-        'kernel',
-        'rootfs',
-        'device',
-    ]
-
-    present = [getattr(arguments, param) is not None for param in parameters]
-
-    if all(present):
-        return True
-    elif any(present) and not all(present):
-        logger.warning("Missing parameters for testing the local image.")
-        for (arg, isPresent) in zip(parameters, present):
-            if isPresent:
-                logger.warning(arg + ": ok")
-            else:
-                logger.warning(arg + ": missing")
-        raise RuntimeError("Some parameters to test a local image are set.")
-
-    return False
-
-
 if __name__ == '__main__':
     # Print out a bash script to call the docker image for lava-ctl
     if len(sys.argv) == 2 and sys.argv[1] == 'bash':
@@ -86,9 +52,12 @@ if __name__ == '__main__':
             sys.stdout.write(f.read())
             exit(0)
 
+    if len(sys.argv) == 2 and sys.argv[1] == '--version':
+        sys.stdout.write(get_version_desc())
+
     import argparse
     parser = argparse.ArgumentParser()
-    sub_cmds = parser.add_subparsers(help='command help')
+    sub_cmds = parser.add_subparsers(help='Command Help')
 
     # Sub-Commands
     commands = [ cmd.Command(logger=logger) for cmd in [submit_job,
@@ -101,33 +70,18 @@ if __name__ == '__main__':
 
     # Overwrite configuration parameters
     parser.add_argument('-p', '--param', dest='conf_params', metavar='KEY=VALUE',
-                        type=str, action='append', help='Set configuration parameter')
-
-    # Submit local files
-    parser.add_argument('--device', metavar='DEVICE',
-                        type=str, help='Device type (e.g. qemux86)')
-    parser.add_argument('--kernel', metavar='FILE',
-                        type=path_exists, help='Kernel to be tested')
-    parser.add_argument('--rootfs', metavar='FILE',
-                        type=path_exists, help='filesystem to be tested')
-
-    # parser.add_argument('--test-repo', dest='test_repos', metavar='URL', action='append',
-    # help='git url for test repository')
-
-    # parser.add_argument('--test-param', dest='test_params', metavar='KEY=VALUE',
-                        # action='append', help='Parameter to make available to the tests')
+                        type=str, action='append', help='set configuration parameter')
 
     # provide a custom configuration file
     parser.add_argument('-c', '--config', metavar='FILE',
-                        type=path_exists, help='Specify a custom configuration file')
+                        type=path_exists, help='specify a custom configuration file')
 
     # Set log level to DEBUG
     parser.add_argument('-d', '--debug', action='store_true',
                         help='Show debug info')
 
     # Print the version to the stdout
-    parser.add_argument('-v', '--version',
-                        action='store_true', help='print version')
+    parser.add_argument('--version', action='store_true', help='print the lava-ctl version')
 
     args = parser.parse_args()
 
@@ -138,8 +92,6 @@ if __name__ == '__main__':
 
     # Print version and exit
     logger.debug("lava-ctl VERSION %s", get_version_desc())
-    if args.version:
-        sys.stdout.write(get_version_desc())
 
     # Init configuration
     config = ConfigManager(filename=args.config, logger=logger)
