631dd6f8d60b1c5b95aa0f16c798b8ee349d7fd2
Alfonso Ros Dos Santos <alfonso.ros-dos-santos@evosoft.com>
2018-02-12 16:28:34 +0100

    Add test validation class

---
+++ lava_ctl/lava/test/schema.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017 Siemens AG
+# Author: Alfonso Ros Dos Santos
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+FILE_URL_REGEX = '^https:\/\/[\w\-\.]+(:[0-9]+)?(\/[\w\-\.\/\+]*)?$'
+GIT_URL_REGEX = '^(https:\/\/|git:\/\/)[@\w\-\.]+(:[0-9]+)?(\/[\w\-\.\/\+]*)?$'
+
+TEST_SCHEMA = {
+    'source': {
+        'type': 'dict',
+        'required': True,
+        'schema': {
+            'device': {
+                'required': True,
+                'type': 'string',
+                'supported_device': True,
+            },
+            'kernel': {
+                'required': True,
+                'excludes': 'image',
+                'dependencies': 'rootfs',
+                'type': 'string',
+                'regex': FILE_URL_REGEX,
+            },
+            'rootfs': {
+                'required': True,
+                'excludes': 'image',
+                'dependencies': 'kernel',
+                'type': 'string',
+                'regex': FILE_URL_REGEX,
+            },
+            'image': {
+                'required': True,
+                'excludes': 'kernel',
+                'type': 'string',
+                'regex': FILE_URL_REGEX,
+            },
+            'rootfs_compressed': {'type': 'boolean'},
+            'patch': {
+                'type': 'dict',
+                'schema': {
+                    'url': {
+                        'required': True,
+                        'type': 'string',
+                        'regex': FILE_URL_REGEX,
+                    },
+                    'target': {
+                        'required': True,
+                        'type': 'string',
+                    },
+                    'partition': {
+                        'required': True,
+                        'type': 'integer',
+                    },
+                }
+            },
+        }
+    },
+    'test_repos': {
+        'type': 'list',
+        'schema': {
+            'type': 'dict',
+            'schema': {
+                'url': {
+                    'required': True,
+                    'type': 'string',
+                    'regex': GIT_URL_REGEX,
+                },
+                'branch': {'type': 'string'},
+                'revision': {'type': 'string'},
+                'tests': {
+                    'required': True,
+                    'type': 'list',
+                    'schema': {
+                      'type': 'string'
+                    },
+                },
+            }
+        }
+    },
+}
